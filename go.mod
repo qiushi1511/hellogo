269fc6bea5a3ca84de3cd7a7a49ff8429f3c0e28
Alex <qiushi1511@gmail.com>
2025-06-25 11:33:27 +0800

    init hellogo app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/qiushi1511/hellogo
+
+go 1.23.1
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	fmt.Println("Router initialized:", r)
+}
