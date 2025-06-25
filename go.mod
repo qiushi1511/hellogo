ecd77c506e463a4a7d844da1cd0a82cf53b4f3b5
alex <qiushi1511@gmail.com>
2025-06-25 16:15:31 +0800

    Introduce hellolib@v2

--- go.mod
+++ go.mod
@@ -1,5 +1,8 @@
 module github.com/qiushi1511/hellogo
 
-go 1.23.1
+go 1.24.4
 
-require github.com/qiushi1511/hellolib/greet v1.0.0
+require (
+	github.com/qiushi1511/hellolib/greet v1.0.0
+	github.com/qiushi1511/hellolib/v2 v2.2.0
+)
--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/qiushi1511/hellolib/greet"
+	hellolib "github.com/qiushi1511/hellolib/v2"
 )
 
 func main() {
 	fmt.Println(greet.Greet("Qiushi"))
 	fmt.Println(greet.WarmGreet("Qiushi"))
+
+	hellolib.Greeting()
 }
